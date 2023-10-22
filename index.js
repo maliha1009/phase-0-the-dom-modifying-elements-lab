d76803e1b5588305f9f4e998cf05a0a9ecbbe8eb
maliha1009 <90807398+maliha1009@users.noreply.github.com>
2023-10-22 12:51:20 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let main = document.querySelector('main#main');
+if (main) {
+    main.parentNode.removeChild(main);
+}
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Maliha is the champion";
+
+document.body.appendChild(newHeader);
